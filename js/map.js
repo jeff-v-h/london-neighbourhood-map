8128e863b361febf7cf588b681fe0a90e9a6c137
Jeffrey Huang <jh0791@gmail.com>
2018-06-17 19:11:44 +0100

    Add temporary data to use

--- js/map.js
+++ js/map.js
@@ -12,6 +12,13 @@ function initMap() {
 
   // A few initial listings of bars to start off with. Later to get data from database 
   var bars = [
-    {title: ''}
-  ]	
+    {title: 'Oriole Bar', location: {lat: 51.518946, lng: -0.102813}},
+    {title: 'Big Easy Bar', location: {lat: 51.510816, lng: -0.122995}},
+    {title: 'Milroy's, location: {lat: 51.514851, lng: -0.131304}},
+    {title: 'The Worlds End', location: {lat: 51.567018, lng: -0.108246}},
+    {title: 'The Faltering Fullback', location: {lat: 51.568619, lng: -0.108182}},
+    {title: 'London Cocktail Club', location: {lat: 51.523940, lng: -0.073050}},
+    {title: 'Bar Kick', location: {lat: 51.526970, lng: -0.078200}},
+    {title: 'Shakespeares Head', location: {lat: 51.513734, lng: -0.139550}}
+  ]	;
 }
