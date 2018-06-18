2e9b6745e176f84b5d3edd2d96caa8e309982195
Jeffrey Huang <jh0791@gmail.com>
2018-06-18 12:21:36 +0100

    Create initial ko ViewModel in separate js file

--- js/map.js
+++ js/map.js
@@ -3,24 +3,24 @@ var map;
 // Create a new blank array for all the listing markers
 var markers = [];
 
+// A few initial listings of bars to start off with. Later to get data from database 
+var bars = [
+  {title: 'Oriole Bar', location: {lat: 51.518946, lng: -0.102813}},
+  {title: 'Big Easy Bar', location: {lat: 51.510816, lng: -0.122995}},
+  {title: 'Milroy\'s', location: {lat: 51.514851, lng: -0.131304}},
+  {title: 'The Worlds End', location: {lat: 51.567018, lng: -0.108246}},
+  {title: 'The Faltering Fullback', location: {lat: 51.568619, lng: -0.108182}},
+  {title: 'London Cocktail Club', location: {lat: 51.523940, lng: -0.073050}},
+  {title: 'Bar Kick', location: {lat: 51.526970, lng: -0.078200}},
+  {title: 'Shakespeares Head', location: {lat: 51.513734, lng: -0.139550}}
+];
+
 function initMap() {
   // Constructor creates a new map
   map = new google.maps.Map(document.getElementById('map'), {
     center: {lat: 51.5088, lng: -0.1268},
     zoom: 13
   });
-
-  // A few initial listings of bars to start off with. Later to get data from database 
-  var bars = [
-    {title: 'Oriole Bar', location: {lat: 51.518946, lng: -0.102813}},
-    {title: 'Big Easy Bar', location: {lat: 51.510816, lng: -0.122995}},
-    {title: 'Milroy\'s', location: {lat: 51.514851, lng: -0.131304}},
-    {title: 'The Worlds End', location: {lat: 51.567018, lng: -0.108246}},
-    {title: 'The Faltering Fullback', location: {lat: 51.568619, lng: -0.108182}},
-    {title: 'London Cocktail Club', location: {lat: 51.523940, lng: -0.073050}},
-    {title: 'Bar Kick', location: {lat: 51.526970, lng: -0.078200}},
-    {title: 'Shakespeares Head', location: {lat: 51.513734, lng: -0.139550}}
-  ];
 
   // Style the markers a bit. This will be the listing marker icon
   var defaultIcon = makeMarkerIcon('0091ff');
---
+++ js/mvvm.js
@@ -0,0 +1,22 @@
+// Separate JavaScript file for the ViewModel. The data is currently in map.js
+
+// A function to create an object with observables for each bar item 
+var Bar = function(data) {
+  var self = this;
+  self.title = ko.observable(data.title);
+  self.location = ko.observable(data.location);
+}
+
+// The View Model to deal with everything that happens on the page
+var ViewModel = function() {
+  var self = this;
+
+  // Initialise an observable array with all the selected bars
+  self.barList = ko.observableArray([]);
+  bars.forEach(function(barItem) {
+    self.barList.push( new Bar(barItem) );
+  });
+}
+
+// Apply bindings to get ViewModel to work
+ko.applyBindings(new ViewModel());
